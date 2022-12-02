572bb2cdf58050d135b1e666dfe746e929dec621
”shokhrukh” <kodirovsohruh41@gmail.com>
2022-12-02 22:03:17 +0300

    second commit for today

--- src/main/java/com/shah/snapin/SnapInApplication.java
+++ src/main/java/com/shah/snapin/SnapInApplication.java
@@ -3,9 +3,17 @@ package com.shah.snapin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class SnapInApplication {
+@SpringBootApplication()
+public class SnapInApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SnapInApplication.class);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(SnapInApplication.class, args);
